716513520cd7803b983ca9d3f8b3a9c95159632e
Rémi Caumette <remicaumette@icloud.com>
2019-01-25 19:53:24 +0100

    tidy dep and update models

--- pkg/models/users.go
+++ pkg/models/users.go
@@ -2,6 +2,7 @@ package models
 
 import (
 	"context"
+	"database/sql"
 	"github.com/google/uuid"
 	"time"
 )
@@ -16,35 +17,7 @@ type User struct {
 	CreatedAt time.Time
 }
 
-func UsersCreate(ctx context.Context, name, email, avatarUrl string, githubId int64, admin bool) (*User, error) {
-	id := uuid.New().String()
-	createdAt := time.Now()
-	if _, err := db.ExecContext(ctx, `
-		INSERT INTO users (id, name, email, avatar_url, github_id, admin, created_at)
-		VALUES ($1, $2, $3, $4, $5, $6, $7)
-	`, id, name, email, avatarUrl, githubId, admin, createdAt); err != nil {
-		return nil, err
-	}
-	return &User{
-		ID:        id,
-		Name:      name,
-		Email:     email,
-		AvatarUrl: avatarUrl,
-		GithubID:  githubId,
-		Admin:     admin,
-		CreatedAt: createdAt,
-	}, nil
-}
-
-func UsersFindByID(ctx context.Context, id string) (*User, error) {
-	rows, err := db.QueryContext(ctx, `
-		SELECT id, name, email, avatar_url, github_id, admin, created_at FROM users
-		WHERE id = $1
-	`, id)
-	if err != nil {
-		return nil, err
-	}
-	defer rows.Close()
+func userFromRows(rows *sql.Rows) (*User, error) {
 	if rows.Next() {
 		user := &User{}
 		if err := rows.Scan(user.ID, user.Name, user.Email, user.AvatarUrl,
@@ -56,10 +29,63 @@ func UsersFindByID(ctx context.Context, id string) (*User, error) {
 	return nil, nil
 }
 
+func UsersFindByID(ctx context.Context, id string) (*User, error) {
+	rows, err := db.QueryContext(ctx, `
+		SELECT id, name, email, avatar_url, github_id, admin, created_at FROM users
+		WHERE id = $1
+	`, id)
+	if err != nil {
+		return nil, err
+	}
+	defer rows.Close()
+	return userFromRows(rows)
+}
+
 func UsersFindByEmail(ctx context.Context, email string) (*User, error) {
-	return nil, nil
+	rows, err := db.QueryContext(ctx, `
+		SELECT id, name, email, avatar_url, github_id, admin, created_at FROM users
+		WHERE email = $1
+	`, email)
+	if err != nil {
+		return nil, err
+	}
+	defer rows.Close()
+	return userFromRows(rows)
 }
 
 func UsersFindByGithubID(ctx context.Context, id int64) (*User, error) {
-	return nil, nil
+	rows, err := db.QueryContext(ctx, `
+		SELECT id, name, email, avatar_url, github_id, admin, created_at FROM users
+		WHERE github_id = $1
+	`, id)
+	if err != nil {
+		return nil, err
+	}
+	defer rows.Close()
+	return userFromRows(rows)
+}
+
+func UsersCreate(ctx context.Context, user *User) error {
+	user.ID = uuid.New().String()
+	user.CreatedAt = time.Now()
+	_, err := db.ExecContext(ctx, `
+		INSERT INTO users (id, name, email, avatar_url, github_id, admin, created_at)
+		VALUES ($1, $2, $3, $4, $5, $6, $7)
+	`, user.ID, user.Name, user.Email, user.AvatarUrl, user.GithubID, user.Admin, user.CreatedAt)
+	return err
+}
+
+func UsersUpdate(ctx context.Context, user *User) error {
+	_, err := db.ExecContext(ctx, `
+		UPDATE users SET name = $2, email = $3, avatar_url = $4, github_id = $5, admin = $6, created_at = $7
+	  	WHERE id = $1
+	`, user.ID, user.Name, user.Email, user.AvatarUrl, user.GithubID, user.Admin, user.CreatedAt)
+	return err
+}
+
+func UsersDelete(ctx context.Context, id string) error {
+	_, err := db.ExecContext(ctx, `
+		DELETE FROM users WHERE id = $1
+	`, id)
+	return err
 }
