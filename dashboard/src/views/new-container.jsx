df35080ac747cc9aa9d374fec20b16990b902989
Rémi Caumette <remicaumette@icloud.com>
2019-02-02 23:24:32 +0100

    fix bug and add new container view

--- dashboard/src/views/new-container.jsx
+++ dashboard/src/views/new-container.jsx
@@ -1,5 +1,6 @@
 import React from 'react'
-import { Form, Input, Select, Button } from 'antd'
+import { Header } from '../components'
+import { Link } from 'react-router-dom'
 
 const formItemLayout = {}
 
@@ -9,26 +10,45 @@ export default (props) => {
     }
 
     return (
-        <div>
-            <h1>Create Containers</h1>
-            <Form onSubmit={onSubmit}>
-                <Form.Item {...formItemLayout} label={'Name'} required>
-                    <Input type={'text'} maxLength={30} />
-                </Form.Item>
-                <Form.Item {...formItemLayout} label={'Image'} required>
-                    <Input type={'text'} maxLength={255} />
-                </Form.Item>
-                <Form.Item {...formItemLayout} label={'Size'} required>
-                    <Select defaultValue={64}>
-                        <Select.Option value={64}>64mb</Select.Option>
-                        <Select.Option value={128}>128mb</Select.Option>
-                        <Select.Option value={256}>256mb</Select.Option>
-                    </Select>
-                </Form.Item>
-                <Form.Item {...formItemLayout}>
-                    <Button type={'primary'} htmlType={'submit'}>Create</Button>
-                </Form.Item>
-            </Form>
+        <div className={'row justify-content-center'}>
+            <div className={'col-12 col-lg-10 col-xl-8'}>
+                <Header pretitle={'Containers'} title={'Create a new container'}/>
+
+                <form className={'mb-5'}>
+                    <div className="form-group">
+                        <label>Name</label>
+                        <input type="text" className="form-control" placeholder={'my-container'}/>
+                    </div>
+
+                    <div className="form-group">
+                        <label>Image</label>
+                        <input type="text" className="form-control" placeholder={'nginx:latest'}/>
+                    </div>
+
+                    <div className="form-group">
+                        <label>Select a size</label>
+                        <select className="form-control">
+                            <option value={'64'}>64mb</option>
+                            <option value={'128'}>128mb</option>
+                            <option value={'256'}>256mb</option>
+                        </select>
+                    </div>
+
+                    <div className="form-group">
+                        <label>Tags</label>
+                        <input type="text" className="form-control"/>
+                    </div>
+
+                    <hr className="mt-5 mb-5" />
+
+                    <button className={'btn btn-block btn-primary'}>
+                        Create container
+                    </button>
+                    <Link to={'/containers'} className={'btn btn-block btn-link text-muted'}>
+                        Cancel
+                    </Link>
+                </form>
+            </div>
         </div>
     )
 }
