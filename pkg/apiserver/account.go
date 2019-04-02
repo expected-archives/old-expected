bd673e10e607e5c8bbd9c5a27804b5cb8027c347
Rémi Caumette <remicaumette@icloud.com>
2019-04-02 15:43:01 +0200

    add regenerate apikey

--- pkg/apiserver/account.go
+++ pkg/apiserver/account.go
@@ -1,13 +1,13 @@
 package apiserver
 
 import (
+	"github.com/expectedsh/expected/pkg/models/accounts"
+	"github.com/sirupsen/logrus"
 	"net/http"
 
 	"github.com/expectedsh/expected/pkg/apiserver/response"
 	"github.com/expectedsh/expected/pkg/apiserver/session"
 )
-
-
 
 func (s *ApiServer) GetAccount(w http.ResponseWriter, r *http.Request) {
 	response.Resource(w, "account", session.GetAccount(r))
@@ -15,12 +15,14 @@ func (s *ApiServer) GetAccount(w http.ResponseWriter, r *http.Request) {
 
 func (s *ApiServer) SyncAccount(w http.ResponseWriter, r *http.Request) {
 
-
 	response.Resource(w, "account", session.GetAccount(r))
 }
 
-func (s *ApiServer) RenewAPIKeyAccount(w http.ResponseWriter, r *http.Request) {
-
-
-	response.Resource(w, "account", session.GetAccount(r))
+func (s *ApiServer) RegenerateAPIKeyAccount(w http.ResponseWriter, r *http.Request) {
+	account := session.GetAccount(r)
+	account.RegenerateAPIKey()
+	if err := accounts.Update(r.Context(), account); err != nil {
+		logrus.WithField("account", account.ID).WithError(err).Errorln("unable to regenerate api key")
+	}
+	response.Resource(w, "account", account)
 }
--- pkg/models/accounts/account.go
+++ pkg/models/accounts/account.go
@@ -1,8 +1,6 @@
 package accounts
 
 import (
-	"context"
-	"github.com/expectedsh/expected/pkg/services"
 	"github.com/google/uuid"
 	"strings"
 	"time"
@@ -20,44 +18,7 @@ type Account struct {
 	CreatedAt         time.Time `json:"created_at"`
 }
 
-func Create(ctx context.Context, name, email, avatarUrl string, githubId int64,
-	githubAccessToken string, admin bool) (*Account, error) {
-	id := uuid.New().String()
-	apiKey := strings.Replace(uuid.New().String(), "-", "", -1)
-	createdAt := time.Now()
-
-	_, err := services.Postgres().Client().ExecContext(ctx, `
-		INSERT INTO accounts (id, name, email, avatar_url, github_id, github_access_token, api_key, admin, created_at)
-		VALUES ($1, $2, $3, $4, $5, $6, $7, $8, $9)
-	`, id, name, email, avatarUrl, githubId, githubAccessToken, apiKey, admin, createdAt)
-
-	return &Account{
-		ID:                id,
-		Name:              name,
-		Email:             email,
-		AvatarURL:         avatarUrl,
-		GithubID:          githubId,
-		GithubAccessToken: githubAccessToken,
-		APIKey:            apiKey,
-		Admin:             admin,
-		CreatedAt:         createdAt,
-	}, err
-}
-
-func Update(ctx context.Context, account *Account) error {
-	_, err := services.Postgres().Client().ExecContext(ctx, `
-		UPDATE accounts SET name = $2, email = $3, avatar_url = $4, github_id = $5, github_access_token = $6,
-		api_key = $7, admin = $8 WHERE id = $1
-	`, account.ID, account.Name, account.Email, account.AvatarURL, account.GithubID, account.GithubAccessToken,
-		account.APIKey, account.Admin)
-
-	return err
-}
-
-func Delete(ctx context.Context, id string) error {
-	_, err := services.Postgres().Client().ExecContext(ctx, `
-		DELETE FROM accounts WHERE id = $1
-	`, id)
-
-	return err
+func (account *Account) RegenerateAPIKey() string {
+	account.APIKey = strings.Replace(uuid.New().String(), "-", "", -1)
+	return account.APIKey
 }
