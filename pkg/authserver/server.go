ceb9b4eac234618d02ec8ac38f16feaff2d65f61
Rémi Caumette <remicaumette@icloud.com>
2019-04-11 08:59:38 +0200

    remove cors middleware

--- pkg/authserver/server.go
+++ pkg/authserver/server.go
@@ -1,7 +1,6 @@
 package authserver
 
 import (
-	"github.com/expectedsh/expected/pkg/util/cors"
 	"github.com/expectedsh/expected/pkg/util/github"
 	"github.com/gorilla/mux"
 	"golang.org/x/oauth2"
@@ -40,9 +39,6 @@ func (s *AuthServer) Start() error {
 
 	router.HandleFunc("/auth/registry", s.AuthRegistry).Methods("GET")
 
-	if err := cors.ApplyMiddleware(router); err != nil {
-		return err
-	}
 	return http.ListenAndServe(s.Addr, router)
 }
 
