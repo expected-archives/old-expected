d9279c749df0cdf40462e7b2521150be5f56a0e2
Rémi Caumette <remicaumette@icloud.com>
2019-04-18 17:51:12 +0200

    add strcut

--- pkg/models/containers/container.go
+++ pkg/models/containers/container.go
@@ -16,11 +16,18 @@ type Container struct {
 	ID          string            `json:"id"`
 	Name        string            `json:"name"`
 	Image       string            `json:"image"`
-	Endpoint    string            `json:"endpoint"`
+	Endpoints   []*Endpoint       `json:"endpoint"`
 	PlanID      string            `json:"plan_id"`
 	Environment map[string]string `json:"environment"`
 	Tags        []string          `json:"tags"`
 	NamespaceID string            `json:"namespace_id"`
 	State       State             `json:"state"`
 	CreatedAt   time.Time         `json:"created_at"`
+}
+
+type Endpoint struct {
+	ID        string    `json:"id"`
+	Endpoint  string    `json:"endpoint"`
+	Default   bool      `json:"default"`
+	CreatedAt time.Time `json:"created_at"`
 }
