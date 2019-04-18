376b89025401a69e9f92cc600dd43aa344fa2fa6
alexisvisco <alexis.viscogliosi@outlook.fr>
2019-04-18 16:45:17 +0200

    fix events

---
+++ migrations/20190417152023_create_events_table.up.sql
@@ -0,0 +1,15 @@
+CREATE TYPE event_resource AS ENUM ('container', 'image', 'plan', 'customplan', 'account');
+CREATE TYPE event_action AS ENUM ('create', 'delete', 'update');
+CREATE TYPE event_issuer AS ENUM ('robot', 'account');
+
+CREATE TABLE events
+(
+    id          UUID           NOT NULL PRIMARY KEY,
+    resource    event_resource NOT NULL,
+    resource_id UUID,
+    action      event_action   NOT NULL,
+    issuer      event_issuer   NOT NULL,
+    issuer_id   UUID,
+    metadata    JSON           NOT NULL,
+    created_at  TIMESTAMP      NOT NULL DEFAULT NOW()
+)
