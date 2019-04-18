7d7f1925e8450272b9751b649e2f1e6e15e47353
alexisvisco <alexis.viscogliosi@outlook.fr>
2019-04-18 16:44:14 +0200

    fix group by to have unique names

--- pkg/models/images/query_image.go
+++ pkg/models/images/query_image.go
@@ -58,7 +58,8 @@ func FindImagesName(ctx context.Context, namespaceId string) ([]string, error) {
 	rows, err := services.Postgres().Client().QueryContext(ctx, `
 		SELECT concat(name, concat(':', tag)) 
 		FROM images 
-		WHERE namespace_id = $1
+		WHERE namespace_id = $1 
+		GROUP BY concat(name, concat(':', tag))
 	`, namespaceId)
 	if err != nil {
 		return nil, err
