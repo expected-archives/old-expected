c64291b7893726f86d403626e68cf76e3ff11fde
Rémi Caumette <remicaumette@icloud.com>
2019-04-26 16:57:05 +0200

    update vagrantfile

--- hack/provision.sh
+++ hack/provision.sh
@@ -4,5 +4,23 @@ apt-get update
 apt-get upgrade -y
 apt-get install -y unzip git build-essential
 
+wget -q https://get.docker.com -O - | sh
+usermod -aG docker vagrant
+
+apt-get install -y docker-compose
+
 wget -q https://storage.googleapis.com/gvisor/releases/nightly/latest/runsc -O /usr/local/bin/runsc
 chmod a+x /usr/local/bin/runsc
+
+wget -q https://dl.google.com/go/go1.12.4.linux-amd64.tar.gz -O go.tar.gz
+tar -C /usr/local -xzf go.tar.gz
+
+cat <<EOF >> ~/.bashrc
+export PATH=\$PATH:/usr/local/go/bin
+export GOPATH=~/go
+export GO111MODULE=on
+export PATH=\$PATH:\$GOPATH/bin
+EOF
+
+docker-compose up -d ~/expected/hack/services.yaml
+
